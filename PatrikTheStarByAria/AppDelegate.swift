69c19363051e72ebff03b60db4eb1b00eafa7d55
Aria Zare <ariaaazare@gmail.com>
2025-08-19 18:39:48 +0200

    Initial Commit

---
+++ PatrikTheStarByAria/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  PatrikTheStarByAria
+//
+//  Created by Aria Zare on 19/08/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ PatrikTheStarByAria/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PatrikTheStarByAria
+//
+//  Created by Aria Zare on 19/08/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
