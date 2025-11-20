e9d79ec785b657090e3190ed6696c792675a8106
Aria Zare <ariaaazare@gmail.com>
2025-11-20 07:46:34 +0100

    [+] added tree behavior to the Node

--- Engine/Node.swift
+++ Engine/Node.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 class Node {
+    weak var parent: Node?
+    var children: [Node] = []
+    
     var rotation: float3 = float3(0, 0, 0) {
         didSet {
             let rotationMatrix = float4x4(rotation: rotation)
@@ -25,10 +28,26 @@ class Node {
         self.quaternion = simd_quatf(rotationMatrix)
     }
     
+    convenience init(parent: Node?) {
+        self.init()
+        self.parent = parent
+    }
+    
     var modelMatrix: float4x4 {
         let rotationMatrix = float4x4(quaternion)
         let transalationMatrix = float4x4(translation: position)
         let scaleMatrix = float4x4(scaling: scale)
         return transalationMatrix * rotationMatrix * scaleMatrix
+    }
+    
+    func addChild(node: Node) {
+        children.append(node)
+        node.parent = self
+    }
+    
+    func removeChild(child: Node) {
+        child.parent = nil
+        //if a child is removed from parent, the grandchildren will not be associated with grand parent(they will stay with parent), to be seen if in the future it would seem problematic or not
+        self.children.removeAll { $0 === child }
     }
 }
