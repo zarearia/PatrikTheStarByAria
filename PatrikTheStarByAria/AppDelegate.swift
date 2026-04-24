29f4ece8d208aeecca7f84685613e487f02f7159
Aria Zare <ariaaazare@gmail.com>
2026-04-24 08:08:04 +0200

    [+] It appears tesselation is my weak point, I'm spending some time reading about it from multiple resources to understand the pipeline better

--- PatrikTheStarByAria/AppDelegate.swift
+++ PatrikTheStarByAria/AppDelegate.swift
@@ -10,7 +10,7 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    // Starting tessalation and heighmap collision(tessalation itself(chapter 11))
+    // It appears tesselation is my weak point, I'm spending some time reading about it from multiple resources to understand the pipeline better
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
