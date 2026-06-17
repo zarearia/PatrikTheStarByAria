6aa5e9f1f0a5887ca274b342e349fb051dfadec3
Aria Zare <ariaaazare@gmail.com>
2026-06-17 08:20:48 +0200

    [*] reviewing morphing and practicing in code

--- PatrikTheStarByAria/AppDelegate.swift
+++ PatrikTheStarByAria/AppDelegate.swift
@@ -10,7 +10,7 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    // adding metalic and routhness for image based lighting
+    // reviewing morphing and practicing in code
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
