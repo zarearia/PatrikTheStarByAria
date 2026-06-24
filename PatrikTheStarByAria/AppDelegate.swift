51492c8a1cee5a5fba2aa45d447fe7e2904a8b34
Aria Zare <ariaaazare@gmail.com>
2026-06-24 08:27:49 +0200

    [+]morphing textures

--- PatrikTheStarByAria/AppDelegate.swift
+++ PatrikTheStarByAria/AppDelegate.swift
@@ -10,7 +10,7 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    // implementing morphing
+    // morphing textures
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
