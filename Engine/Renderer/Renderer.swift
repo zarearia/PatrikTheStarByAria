9697b208b73a70d2cabc8748ded4a5848ce6f427
Aria Zare <ariaaazare@gmail.com>
2026-07-01 08:01:15 +0200

    [+] made shadow texture and renderpass description

--- Engine/Renderer/Renderer.swift
+++ Engine/Renderer/Renderer.swift
@@ -23,6 +23,9 @@ class Renderer: NSObject {
     var texture: MTLTexture
     
     var brdfLut: MTLTexture
+    
+    var shadowTexture: MTLTexture!
+    var shadowRenderPassDescription: MTLRenderPassDescriptor = MTLRenderPassDescriptor()
 
     init(metalView: MTKView) {
         guard
@@ -59,6 +62,8 @@ class Renderer: NSObject {
         
         metalView.sampleCount = Renderer.rasterSampleCount
         
+        updateShadowTextureAndRenderPassDescriptor(width: Int(metalView.drawableSize.width), height: Int(metalView.drawableSize.height))
+        
     }
     
     
@@ -69,12 +74,32 @@ class Renderer: NSObject {
         
         depthStencilState = Renderer.device.makeDepthStencilState(descriptor: descriptor)
     }
+    
+    static func makeTexture(pixelFormat: MTLPixelFormat,
+                     width: Int,
+                     height: Int,
+                     label: String) -> MTLTexture {
+        let descriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: pixelFormat, width: width, height: height, mipmapped: false)
+        descriptor.usage = [.shaderRead, .renderTarget]
+        descriptor.storageMode = .private
+        guard let texture = Renderer.device.makeTexture(descriptor: descriptor) else {
+            fatalError("could not make texture: \(label)")
+        }
+        texture.label = label
+        return texture
+    }
+    
+    func updateShadowTextureAndRenderPassDescriptor(width: Int, height: Int) {
+        self.shadowTexture = Renderer.makeTexture(pixelFormat: .depth32Float, width: width, height: height, label: "shadow")
+        shadowRenderPassDescription.setupDepthAttachment(texture: self.shadowTexture)
+    }
 }
 
 
 extension Renderer: MTKViewDelegate {
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
         scene?.sceneSizeWillChange(newSize: size)
+        updateShadowTextureAndRenderPassDescriptor(width: Int(size.width), height: Int(size.height))
     }
     
     func draw(in view: MTKView) {
@@ -124,5 +149,14 @@ extension Renderer: MTKViewDelegate {
         commandBuffer.present(drawable)
         commandBuffer.commit()
         commandBuffer.waitUntilCompleted()
+    }
+}
+
+extension MTLRenderPassDescriptor {
+    func setupDepthAttachment(texture: MTLTexture) {
+        self.depthAttachment.texture = texture
+        self.depthAttachment.clearDepth = 1
+        self.depthAttachment.loadAction = .clear
+        self.depthAttachment.storeAction = .store
     }
 }
